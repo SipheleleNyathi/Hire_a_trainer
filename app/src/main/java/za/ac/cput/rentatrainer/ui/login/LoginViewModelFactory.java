a59ad50adbe6937bc76f77b4008ee38522e4ad4e
Shyam Prag <pragshyam11@gmail.com>
2021-05-06 21:46:26 +0200

    initial commit

---
+++ app/src/main/java/za/ac/cput/rentatrainer/ui/login/LoginViewModelFactory.java
@@ -0,0 +1,26 @@
+package za.ac.cput.rentatrainer.ui.login;
+
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.annotation.NonNull;
+
+import za.ac.cput.rentatrainer.data.LoginDataSource;
+import za.ac.cput.rentatrainer.data.LoginRepository;
+
+/**
+ * ViewModel provider factory to instantiate LoginViewModel.
+ * Required given LoginViewModel has a non-empty constructor
+ */
+public class LoginViewModelFactory implements ViewModelProvider.Factory {
+
+    @NonNull
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+        if (modelClass.isAssignableFrom(LoginViewModel.class)) {
+            return (T) new LoginViewModel(LoginRepository.getInstance(new LoginDataSource()));
+        } else {
+            throw new IllegalArgumentException("Unknown ViewModel class");
+        }
+    }
+}
